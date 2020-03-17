db1f1e81ee7331d4a08128fa0744ea671dcc16f2
twp <767339439@qq.com>
2020-03-17 17:43:26 +0800

    西交，西财，修改check和rejister

--- app/schools/xinanjiaotong/xinanjiaotong_check.py
+++ app/schools/xinanjiaotong/xinanjiaotong_check.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import requests, re
+import requests
+from app.schools.models import Company
 
 # 检查企业是否存在
 def check(account):
@@ -15,6 +16,9 @@ def check(account):
         "Content-Type": "text/plain"
     }
 
+    # 获取企业信息
+    company = Company.query.filter(Company.name == account).first()
+
     formdata = {
         "callCount": "1",
         "page": "/eweb/jygl/jyglext.so?type=dwzc",
@@ -25,18 +29,26 @@ def check(account):
         "c0-id": "0",
         "c0-param0": "string:Bczoygd2fM6bfagjF8ZNRt",
         "c0-param1": "string:" + account,
-        "c0-param2": "string:",
-        "c0-param3": "string:",
+        "c0-param2": "string:" + company.credit_num,
+        "c0-param3": "string:" + company.contact_person_email,
         "c0-param4": "string:xzDwxx",
         "c0-param5": "boolean:false",
         "batchId": "2"
     }
 
     response = requests.post(url, headers=headers, data=formdata)
-    print(re.search('zpbmemailResult:"true",dwmcResult:"dwusertrue",zzjgdmfjResult:"true",zzjgdmResult:"true"', response.text).span())
-    print(response.text)
-
+    if 'dwmcResult:"dwusertrue"' in response.text:
+        return {'code': 0, 'msg': account + '已被注册'}
+    elif 'zzjgdmResult:"zzjgdmfalse"' in response.text:
+        return {'code': 2, 'msg': account + '的企业信用代码已被注册'}
+    elif 'zpbmemailResult:"dwzhfalse"' in response.text:
+        return {'code': 3, 'msg': account + "登陆西南交大的email"+ "已被使用"}
+    elif 'zpbmemailResult:"true"' in response.text and 'dwmcResult:"true"' in response.text and 'zzjgdmResult:"true"' in response.text:
+        # print(1)
+        return {'code': 1, 'msg': account + '可注册'}
+    else:
+        return {'code': 5, 'msg': '网络异常'}
 
 if __name__ == '__main__':
     # check('神州优车股份有限公司', 'szyc2018')
-    check('神州优车股份有限公司')
+    print(check('神州优车股份有限公')['code'])
